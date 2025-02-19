989060e06e12529574e08f11c90431d49c933881
palak033 <gkpalak033@gmail.com>
2025-02-19 22:34:14 +0530

    Assignment_Closures1

---
+++ Assignment_Closure1/index.js
@@ -0,0 +1,47 @@
+// 1st Counter Module using Closures
+
+
+
+// function createCounter() {
+//     let count = 0;
+
+//     return {
+//         increment() {
+//             console.log("Counter:", ++count);
+//         },
+//         decrement() {
+//             console.log("Counter:", --count);
+//         },
+//         getCount() {
+//             return count;
+//         }
+//     };
+// }
+
+// const counter = createCounter();
+// counter.increment();
+// counter.increment();
+// counter.decrement();
+// console.log("Current Count:", counter.getCount());
+
+
+
+
+// Memoized Fibonacci Function Using Closures
+
+function memoizedFibonacci() {
+    let cache = {};
+
+    return function fib(n) {
+        if (n <= 1) return n;
+        if (cache[n]) return cache[n];
+        
+        return cache[n] = fib(n - 1) + fib(n - 2);
+    };
+}
+
+const fibonacci = memoizedFibonacci();
+console.log(fibonacci(10)); // 55
+console.log(fibonacci(20)); // 6765
+console.log(fibonacci(30)); // 832040
+
