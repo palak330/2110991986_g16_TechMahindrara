13fc81041c13cb2da7bba462ba323ea1407f65df
palak033 <gkpalak033@gmail.com>
2025-02-21 10:33:42 +0530

    classwork

--- Lec10_20022025/index.js
+++ Lec10_20022025/index.js
@@ -60,11 +60,90 @@
 // tech();
 
 
-async function tech(){
-    console.log("hello");
+
+
+
+
+// async function tech(){
+//     console.log("hello");
+// }
+// const techData=tech();
+//   console.log(tech);
+// techData.then((res)=>{
+//     return res;
+// })
+
+
+
+
+//==============================================
+// console.log("start");
+// const project=new Promise(function(res,rej){
+//     setTimeout(()=>{
+//         res("submitted");
+//     },5000)
+// });
+// console.log("class lunch");
+
+// async function evaluation(){
+//     console.log("attendance viva");
+//     const projectfile=await project;
+//      console.log(projectfile);
+//     console.log("project file check");
+// }
+
+// evaluation();
+// console.log("class end");
+
+
+
+
+//========================================================
+//question
+// console.log("class start"); // Step 1: Print "class start"
+
+// const project = new Promise(function (res, rej) {
+//     setTimeout(() => {
+//         res("writting file");
+//     }, 1000); // Step 2: Resolve after 1 seconds
+// });
+
+// console.log("Attendance Marking"); // Step 3: Print "Attendance Marking"
+
+// async function evaluation() {
+//     const p = await project; // Waits 10 seconds
+//     console.log(p); // Step 2: Print "writting file" after 10 sec
+
+//     console.log("file check evaluation"); // Step 4: Print "file check evaluation" after 10 sec
+// }
+
+// evaluation(); // Start async function
+
+// // Do not print "class end", instead print "end class"
+// setTimeout(() => {
+//     console.log("end class"); // Step 6: Print "end class" after 1 sec
+// }, 1000);
+
+
+
+
+
+
+
+async function fetchApi(){
+    try{
+    const data=await fetch("link likho");
+    const mydata=await datajson();
+    return mydata;
 }
-const techData=tech();
-  console.log(tech);
-techData.then((res)=>{
-    return res;
+catch(err){
+    console.log("err");
+}
+}
+
+const finalData=fetchApi();
+
+finalData.then((res)=>{
+    console.log(res);
 })
+
---
+++ Lec11_21022025/index.js
@@ -0,0 +1,80 @@
+//call()
+
+//1
+	// const obj = { 
+	// 	firstName: "Pretti", 
+	// 	lastName: "raj", 
+	// 	printName: function () { 
+	// 		console.log(this.firstName + " " + this.lastName); 
+	// 	} 
+	// }; 
+	// obj.printName(); 
+
+
+
+   //2
+	// const obj1 = { 
+	// 	firstName: "riya", 
+	// 	lastName: "sharma"
+	// }; 
+	// const obj2 = { 
+	// 	firstName: "Sachin", 
+	// 	lastName: "Tendulkar"
+	// }; 
+	// function printName() { 
+	// 	console.log(this.firstName + " " + this.lastName); 
+	// } 
+	// printName.call(obj2); 
+
+
+
+    //apply
+   
+	// const obj1 = { 
+	// 	firstName: "pretti", 
+	// 	lastName: "raj"
+	// }; 
+	// const obj2 = { 
+	// 	firstName: "Sachin", 
+	// 	lastName: "Tendulkar"
+	// }; 
+	// function printName() { 
+	// 	console.log(this.firstName + " " + this.lastName); 
+	// } 
+	// printName.apply(obj2); 
+
+
+//bind
+// const person = {
+//     firstName:"John",
+//     lastName: "Doe",
+//     fullName: function () {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   }
+  
+//   const member = {
+//     firstName:"Hege",
+//     lastName: "Nilsen",
+//   }
+  
+//   let fullName = person.fullName.bind(member);
+
+
+
+
+document.getElementById("grandParent").addEventListener("click",()=>{
+    console.log("This is grand Parent div");
+},true)
+
+document.getElementById("Parent").addEventListener("click",()=>{
+    console.log("This is Parent div");
+},true)
+
+document.getElementById("child").addEventListener("click",()=>{
+    console.log("This is child div");
+},true)
+
+   
+
+
